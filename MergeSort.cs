104eb805b298b0e529229b748e2d76237f768e0e
Alicia W <rainingilove@gmail.com>
2014-10-17 18:42:59 +0200

    Stream IO examples added.

---
+++ MergeSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+
+namespace Algorithms
+{
+
+	class Program
+	{
+		static void Main()
+		{
+			string filename = "../../data/tinyW.txt";
+			TestReadAndWrite.ReadFileTest (filename);
+		}
+	}
+	/// <summary>
+	/// Implementation of the algorithm merge sort.
+	/// </summary>
+	public class MergeSort
+	{
+		public MergeSort ()
+		{
+		}
+	}
+
+	public class TestReadAndWrite
+	{
+		public static void Test()
+		{
+			using (Stream s = new FileStream ("test.txt", FileMode.Create)) 
+			{
+				Console.WriteLine (s.CanRead);
+				Console.WriteLine (s.CanWrite);
+				Console.WriteLine (s.CanSeek);
+
+				s.WriteByte (101);
+				byte[] block = { 1, 2, 3, 4, 5 };
+				s.Write (block, 0, block.Length);
+
+				Console.WriteLine (s.Length);
+				Console.WriteLine (s.Position);
+
+				s.Position = 0; // Move back to the start
+
+				byte[] block2 = new byte[5];
+				Console.WriteLine (s.ReadByte ());
+				Console.WriteLine (s.Read (block2, 0, block2.Length)); 
+				Console.WriteLine (block2[2]);
+			}
+		}
+
+		public static void ReadFileTest(string filename)
+		{
+			using (FileStream fs = File.OpenRead (filename))
+			using (TextReader reader = new StreamReader (fs)) 
+			{
+				while (reader.Peek () > -1) {
+					Console.WriteLine (reader.ReadLine ());
+				}
+			}
+		}
+	}
+}
+
--- hello_simple.cs
+++ hello_simple.cs
@@ -2,8 +2,13 @@ using System;
 
 public class HelloWorld
 {
+	/// <summary>
+	/// The entry point of the program, where the program control starts and ends.
+	/// Every C# application must contain a single Main method specifying where 
+	/// program execution is to begin.
+	/// </summary>
     static public void Main ()
     {
-        Console.WriteLine("Hello World !\n");
+        Console.WriteLine ("Hello World !\n");
     }
 }
